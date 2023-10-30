14a6f5c2c83093aafe5b0e112c536a2fa2e92c6e
Carlos G <ca.garciag1@uniandes.edu.co>
2023-10-29 21:15:41 -0500

    front finished

--- front_app/src/InputForm.js
+++ front_app/src/InputForm.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import axios from "axios";
 import "./InputForm.css"
 
 function InputForm() {
   const [inputString, setInputString] = useState("");
   const [result, setResult] = useState("");
+  const [results, setResults] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post("http://127.0.0.1:8000/convert", { "input_string": inputString });
       setResult(response.data.predict);
+      setResults([...results, response.data.predict]);
     } catch (error) {
       console.error("Error:", error);
     }
@@ -21,6 +23,7 @@ function InputForm() {
 		setInputString(newName);
 	});
 
+  const truncatedString = inputString.slice(0, 20);
 
   return (
     <div className="centered-container">
@@ -35,7 +38,11 @@ function InputForm() {
         />
         <button onClick={handleSubmit}>Submit</button>
       </form>
-      <p className="result-text">Result: {result}</p>
+      {results.map((item, index) => (
+        <p key={index} className="result-text">
+          El texto es afin al ODS:   {item}
+        </p>
+      ))}
     </div>
   );
 }
